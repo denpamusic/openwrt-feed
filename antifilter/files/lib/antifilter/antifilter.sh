d765de27ee43d65f9c48fbaeb5a072077c3101fd
denpamusic <denpa@denpa.pro>
2021-09-23 03:03:06 +0300

    [antifilter] minor optimizations

--- antifilter/files/lib/antifilter/antifilter.sh
+++ antifilter/files/lib/antifilter/antifilter.sh
@@ -12,84 +12,89 @@ log() {
 	local level="$1"
 	local message="$2"
 
-	[ "$level" = "error" ] && >&2 echo "$message" || echo "$message"
+	case $level in
+		warning | error | debug)
+			>&2 echo "$message"
+			;;
+		*)
+			echo "$message"
+			;;
+	esac
+
 	logger -t "antifilter[$$]" -p "daemon.$1" "$2"
 }
 
 count_ipset_entries() {
-	$IPSETQ -t list "$1" | tail -1 | cut -f2 -d':' | xargs
+	$IPSETQ -t list "$1" | tail -1 | cut -f2 -d":" | xargs
 }
 
 get_ipset_diff_message() {
 	local before="$1"
 	local after="$2"
 
-	[ -z "$before" ] && before=0
-	[ -z "$after" ] && after=0
+	[ -z $before ] && before=0
+	[ -z $after  ] && after=0
 
-	diff=$(( $before - $after ))
+	diff=$(( before - after ))
 
-	if [ "$diff" -lt 0 ]; then
-		echo added "${diff#-}" entries
-	elif [ "$diff" -gt 0 ]; then
-		echo removed "$diff" entries
-	else
-		echo same entries count
-	fi
+	[ $diff -lt 0 ] && echo added ${diff#-} entries
+	[ $diff -gt 0 ] && echo removed $diff entries
+	[ $diff -eq 0 ] && echo same entries count
 }
 
 get_ipset_name() {
+	local config="$1"
 	local prefix
-	config_get prefix "$1" prefix
-	echo "${prefix}_$1"
+	config_get prefix "$config" prefix
+	echo "$prefix"_"$config"
 }
 
 resolve_hostname() {
-	nslookup "$1" 2>/dev/null | grep -E "Address [0-9]+: $IPV4_PATTERN" | cut -f2 -d':' | xargs
+	local hostname="$1"
+	local ips
+	ips=$(nslookup "$hostname" 2>/dev/null | grep -E "Address [0-9]+: $IPV4_PATTERN" | cut -f2 -d":" | xargs)
+	[ -z "$ips" ] && return 1
+	echo "$ips"
 }
 
 create_ipset() {
-	local config="$1"
-	local ipset type
+	local name="$1"
+	local type="$2"
 
-	config_get type "$config" type "hash:net"
-
-	ipset=$(get_ipset_name "$config")
-	echo "create ${ipset}_new $type"
-	sed -e "s/^/add ${ipset}_new /"
+	echo "create ${name}_new $type"
+	sed -e "s/^/add ${name}_new /"
 }
 
 load_ipset() {
 	local config="$1"
-	local ipset type state before after
+	local ipset type before after
 
 	config_get type "$config" type "hash:net"
 
-	state=$(create_ipset "$config")
-	echo "$state" | grep -q "add " || return 1
+	name=$(get_ipset_name "$config")
+	ipset=$(create_ipset "$name" "$type")
+	echo "$ipset" | grep -q "add " || return 1
 
-	ipset=$(get_ipset_name "$config")
-	log info "Loading $ipset..."
-	before=$(count_ipset_entries "$ipset")
-	echo "$state" | $IPSETQ restore
-	$IPSETQ create "$ipset" "$type"
-	$IPSETQ swap "${ipset}_new" "$ipset"
-	$IPSETQ destroy "${ipset}_new"
-	after=$(count_ipset_entries "$ipset")
+	before=$(count_ipset_entries "$name")
 
-	log info "$ipset: $(get_ipset_diff_message "$before" "$after")"
-	unset state
+	log info "Loading $name [type: $type]..."
+	echo "$ipset" | $IPSETQ restore
+	$IPSETQ create "$name" "$type"
+	$IPSETQ swap "${name}_new" "$name"
+	$IPSETQ destroy "${name}_new"
+
+	after=$(count_ipset_entries "$name")
+
+	log info "$name: $(get_ipset_diff_message $before $after)"
+	unset ipset
 }
 
 handle_ipset() {
 	local config="$1"
 	local prefix enabled
 
-	config_get_bool enabled "$config" "enabled" 0
-	[ "$enabled" -eq 0 ] && return
-
-	config_get prefix "$config" prefix
-	LOADED="$LOADED ${prefix}_${config}"
+	config_get_bool enabled "$config" enabled 0
+	[ $enabled -eq 1 ] && LOADED="$LOADED $(get_ipset_name "$config")"
 }
 
 get_ipsets() {
@@ -103,10 +108,10 @@ get_ipsets() {
 
 handle_source() {
 	local config="$1"
-	local file sources enabled
+	local file source sources enabled
 
-	config_get_bool enabled "$config" "enabled" 0
-	[ "$enabled" -eq 0 ] && return
+	config_get_bool enabled "$config" enabled 0
+	[ $enabled -eq 0 ] && return
 
 	config_get file "$config" file
 	config_get sources "antifilter" source
@@ -124,69 +129,67 @@ antifilter_update() {
 }
 
 antifilter_remove() {
+	local ipset
 	for ipset in $(get_ipsets); do
 		$IPSETQ destroy "$ipset"
 	done
 }
 
 antifilter_status_ipsets() {
-	local entries
-
-	echo "Loaded ipsets:"
+	local ipset
+	echo Loaded ipsets:
 	for ipset in $(get_ipsets); do
-		entries="$(count_ipset_entries "$ipset")"
-		[ -n "$entries" ] && echo "$ipset ($entries entries)"
+		echo "$ipset ($(count_ipset_entries "$ipset") entries)"
 	done
 }
 
 antifilter_status() {
-	[ -f /var/run/antifilter.pid ] && {
-		echo "Update service is running with pid $(cat /var/run/antifilter.pid)"
-	} || echo "Update service is NOT running"
-
+	[ -f /var/run/antifilter.pid ] &&
+		echo "Update service is running with pid $(cat /var/run/antifilter.pid)" ||
+		echo "Update service is NOT running"
 	echo
 
 	antifilter_status_ipsets
 }
 
 antifilter_dump() {
+	local ipset
 	for ipset in $(get_ipsets); do
 		$IPSETQ save "$ipset"
 	done
 }
 
 antifilter_lookup() {
-	local needle="$*"
-	local resolved=0
+	local ips="$*"
 	local matches=0
-	local found=0
-	local hostname
+	local ip ipsets
 
-	echo "$needle" | grep -Eqv "^($IPV4_PATTERN ?){0,}$" && {
-		hostname="$1"
-		needle=$(resolve_hostname "$hostname") && resolved=1
-	} || hostname="$needle"
+	[ -z $ips ] && return 1
 
-	for ipset in $(get_ipsets); do
-		found=0
-		for ip in $needle; do
-			$IPSET test "$ipset" "$ip" && found=1
+	for ip in $ips; do
+
+		echo "$ip" | grep -Eqv "$IPV4_PATTERN" && {
+			echo "Checking for $ip:"
+			antifilter_lookup $(resolve_hostname "$ip") || echo "error: $ip not found"
+			continue
+		}
+
+		for ipset in $(get_ipsets); do
+			$IPSETQ test "$ipset" "$ip" && matches=$(( matches + 1 )) && ipsets="$ipsets, $ipset"
 		done
-		[ "$found" -eq 1 ] && matches=$(( matches + 1 ))
+
+		[ $matches -gt 0 ] &&
+			echo "$ip is LISTED in following blocklists: ${ipsets:2}." ||
+			echo "$ip is NOT LISTED in any blocklists."
+
 	done
-
-	[ "$matches" -gt 0 ] && {
-		echo "$hostname" is LISTED in "$matches" blocklists.
-	} || echo "$hostname" is NOT LISTED in any blocklists.
-
-	return "$matches"
 }
 
 antifilter_daemon() {
 	local minutes
 
 	config_load antifilter
-	config_get minutes "antifilter" interval 360
+	config_get minutes antifilter interval 360
 
 	log notice "Using update interval of $minutes minutes..."
 	while true; do
