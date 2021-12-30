7dbeee5ca7a62597e44acb1d29de91afe3d38e9f
denpamusic <denpa@denpa.pro>
2021-12-30 22:05:03 +0300

    [antifilter] move utils to separate file

--- antifilter/files/usr/lib/antifilter/antifilter.sh
+++ antifilter/files/usr/lib/antifilter/antifilter.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 . /lib/functions.sh
+. /lib/antifilter/utils.sh
 
 UCLIENT="uclient-fetch -qT 5 -O -"
 IPSET="ipset -!"
@@ -8,95 +9,12 @@ IPSETQ="ipset -! -q"
 NSLOOKUP=/usr/bin/nslookup
 IPV4_PATTERN="[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}"
 PIDFILE=/var/run/antifilter.pid
-DAEMON=0
 LOADED=
-
-__true() {
-	return 0
-}
-
-__false() {
-	return 1
-}
 
 __ipsets() {
 	if_empty "$LOADED" && config_foreach handle_loaded ipset
 
 	echo "$LOADED"
-}
-
-log() {
-	local level="$1"
-	local message="$2"
-
-	if_empty "$message" && message="$level" && level="info"
-
-	case "$level" in
-		warning|info|notice|debug)
-			if_daemon &&
-				logger -t "antifilter[$$]" -p "daemon.$level" "$message" ||
-				echo -e "$message"
-
-			return $(__true)
-			;;
-		error)
-			if_daemon &&
-				logger -t "antifilter[$$]" -p "daemon.error" "$message" ||
-				>&2 echo -e "error: $message"
-
-			return $(__false)
-			;;
-	esac
-}
-
-error() {
-	log error "$1"
-}
-
-if_daemon() {
-	[ "$DAEMON" -eq 1 ]
-}
-
-if_has_ip4_address() {
-	local case="$1"
-
-	echo "$case" | grep -Eq "$IPV4_PATTERN"
-}
-
-if_ipset_exists() {
-	local name="$1"
-
-	$IPSETQ list "$name" >/dev/null 2>&1
-}
-
-if_file_older_than() {
-	local file="$1"
-	local age="$2"
-	local mtime
-
-	[ ! -f "$file" ] && return $(__true)
-
-	mtime=$(date -r "$file" "+%s")
-	now=$(date "+%s")
-
-	[ $(( (now - mtime) / 60 )) -ge $age ]
-}
-
-if_empty() {
-	local vars="$*"
-	local var result
-
-	for var in $vars; do
-		[ -z "$var" ] && return $(false)
-	done
-}
-
-if_enabled() {
-	local config="$1"
-	local enabled
-
-	config_get_bool enabled "$config" enabled 0
-	[ $enabled -eq 1 ]
 }
 
 handle_loaded() {
@@ -126,14 +44,6 @@ count_ipset_entries() {
 
 	if_ipset_exists "$ipset" && count=$($IPSETQ -t list "$ipset" | tail -1 | cut -f2 -d":" | xargs)
 	echo $count
-}
-
-resolve_hostname() {
-	local hostname="$1"
-	local ips=$($NSLOOKUP "$hostname" 2>/dev/null | grep -E "Address [0-9]+: $IPV4_PATTERN" | cut -f2 -d":" | xargs)
-
-	if_empty "$ips" && return $(error "$hostname not found")
-	echo "$ips"
 }
 
 create_ipset() {
---
+++ antifilter/files/usr/lib/antifilter/utils.sh
@@ -0,0 +1,93 @@
+#!/bin/sh
+
+DAEMON=0
+
+__true() {
+	return 0
+}
+
+__false() {
+	return 1
+}
+
+log() {
+	local level="$1"
+	local message="$2"
+
+	if_empty "$message" && message="$level" && level="info"
+
+	case "$level" in
+		warning|info|notice|debug)
+			if_daemon &&
+				logger -t "antifilter[$$]" -p "daemon.$level" "$message" ||
+				echo -e "$message"
+
+			return $(__true)
+			;;
+		error)
+			if_daemon &&
+				logger -t "antifilter[$$]" -p "daemon.error" "$message" ||
+				>&2 echo -e "error: $message"
+
+			return $(__false)
+			;;
+	esac
+}
+
+error() {
+	log error "$1"
+}
+
+if_ipset_exists() {
+	local name="$1"
+
+	$IPSETQ list "$name" >/dev/null 2>&1
+}
+
+if_has_ip4_address() {
+	local case="$1"
+
+	echo "$case" | grep -Eq "$IPV4_PATTERN"
+}
+
+if_daemon() {
+	[ "$DAEMON" -eq 1 ]
+}
+
+if_file_older_than() {
+	local file="$1"
+	local age="$2"
+	local mtime
+
+	[ ! -f "$file" ] && return $(__true)
+
+	mtime=$(date -r "$file" "+%s")
+	now=$(date "+%s")
+
+	[ $(( (now - mtime) / 60 )) -ge $age ]
+}
+
+if_empty() {
+	local vars="$*"
+	local var result
+
+	for var in $vars; do
+		[ -z "$var" ] && return $(false)
+	done
+}
+
+if_enabled() {
+	local config="$1"
+	local enabled
+
+	config_get_bool enabled "$config" enabled 0
+	[ $enabled -eq 1 ]
+}
+
+resolve_hostname() {
+	local hostname="$1"
+	local ips=$($NSLOOKUP "$hostname" 2>/dev/null | grep -E "Address [0-9]+: $IPV4_PATTERN" | cut -f2 -d":" | xargs)
+
+	if_empty "$ips" && return $(error "$hostname not found")
+	echo "$ips"
+}
